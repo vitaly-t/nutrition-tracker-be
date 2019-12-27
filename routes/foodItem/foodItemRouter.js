269e271da35614d2cb1fbaf02e076458e48b64f4
Dave Vazquez <dmvaz.li@gmail.com>
2019-12-26 22:43:39 -0500

    abstracts middle-ware into separate function

--- routes/foodItem/foodItemRouter.js
+++ routes/foodItem/foodItemRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const db = require("./foodItemDB");
 const { getFoodHandler } = require("../fatsecret/fatsecret");
+const db = require("./foodItemDB");
 const mapFirebaseIDtoUserID = require("../../middleware/mapFirebaseIDtoUserID");
 
 router = express.Router();
@@ -21,25 +21,7 @@ router.get(
       });
     }
   },
-  async (req, res) => {
-    let foodItem = res.locals.foodItem;
-
-    try {
-      req.params.food_id = foodItem.fatsecret_food_id;
-      res.locals.returnData = true;
-
-      const fatSecretFoodData = await getFoodHandler(req, res);
-
-      res.status(200).json({
-        foodItem,
-        fatSecretFoodData
-      });
-    } catch (err) {
-      res.status(500).json({
-        errorMessage: "Could not retrieve fat secret food data"
-      });
-    }
-  }
+  getFatSecretData
 );
 
 router.put(
@@ -50,7 +32,7 @@ router.put(
     const updatedRecord = req.body;
 
     try {
-      // call to db to update item;
+      //call to db to update item;
       const item = await db.updateFoodItem(foodLogID, user_id, updatedRecord);
       res.status(201).json(item);
     } catch ({ message }) {
@@ -64,7 +46,6 @@ router.delete(
   mapFirebaseIDtoUserID,
   async (req, res) => {
     const { foodLogID, user_id } = req.params;
-
     try {
       // call to db to delete record;
       const deletedRecord = await db.deleteFoodItem(foodLogID, user_id);
@@ -75,24 +56,26 @@ router.delete(
   }
 );
 
-function getFatSecretData(req, res) {
-  const food_id = req.param.food_id;
+/********************************************************
+ *                  GET FAT SECRET DATA                 *
+ ********************************************************/
+async function getFatSecretData(req, res) {
+  const foodItem = res.locals.foodItem;
+  req.params.food_id = foodItem.fatsecret_food_id;
+  res.locals.returnData = true;
 
-  req.url = `/fatsecret/get-food/${food_id}`;
+  try {
+    const fatSecretFoodData = await getFoodHandler(req, res);
 
-  return express.handle(req, res);
+    res.status(200).json({
+      foodItem,
+      fatSecretFoodData
+    });
+  } catch (err) {
+    res.status(500).json({
+      errorMessage: "Could not retrieve fat secret food data"
+    });
+  }
 }
 
 module.exports = router;
-
-// const { fatsecret_food_id } = foodItem;
-//now that we have our data from the db we need to go and get the fatsecret_food_id for this record and return that info.
-// try {
-//   var data;
-//   await fetch(
-//     `${BASE_URL}/fatsecret/get-food/${fatsecret_food_id}` //make a fetch request from our api and and get info
-//   )
-//     .then(checkStatus)
-//     .then(res => res.json())
-//     .then(json => (data = json)); // json is the actaul data being returned from out api call
-//   const dataAtIndexOne = data[0];
